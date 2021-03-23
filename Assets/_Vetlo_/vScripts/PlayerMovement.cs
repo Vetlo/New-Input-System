e7833df40bb95247dccd36d158fc02c5835b1e1e
Vetlo <vetlo.work@gmail.com>
2021-03-23 18:51:42 +0300

    update

--- Assets/_Vetlo_/vScripts/PlayerMovement.cs
+++ Assets/_Vetlo_/vScripts/PlayerMovement.cs
@@ -5,15 +5,32 @@ public class PlayerMovement : MonoBehaviour
 [SerializeField]
 private float _moveSpeed;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+
 
     // Update is called once per frame
     void Update()
     {
+        float horizontalDirection = Input.GetAxis("Horizontal");
+        float verticalDirection = Input.GetAxis("Vertical");
         
+        Vector2 direction = new Vector2(horizontalDirection, verticalDirection);
+        Move(direction);
+        if (Input.GetMouseButtonDown(0))
+        {
+         Shoot();   
+        }
+
+    }
+
+    void Move(Vector2 direction)
+    {
+        float scaleMoveSpeed = _moveSpeed * Time.deltaTime;
+        Vector3 moveDirection = new Vector3(direction.x, 0, direction.y);
+        transform.position += moveDirection * scaleMoveSpeed;
+    }
+
+    private void Shoot()
+    {
+        Debug.Log("");
     }
 }
