3f049f188e83c0d633ea3948c32ad3f11c16a2d0
Vetlo <vetlo.work@gmail.com>
2021-03-23 17:21:45 +0300

    Добавил скрипт управления

---
+++ Assets/_Vetlo_/vScripts/PlayerMovement.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+[SerializeField]
+private float _moveSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
